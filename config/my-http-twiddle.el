8c636a88dd962646996cd1f20dcac7af6351355d
Christopher Brown <cb@opscode.com>
2011-05-25 11:02:51 -0700

    initial checkin

---
+++ config/my-http-twiddle.el
@@ -0,0 +1,2 @@
+(autoload 'http-twiddle-mode "http-twiddle" "HTTP Twiddle Mode" t nil)
+(add-to-list 'auto-mode-alist '("\\.http-twiddle$" . http-twiddle-mode))
---
+++ config/my-opscode-sign.el
@@ -0,0 +1,10 @@
+;; Load the Opscode signing library
+(load-library "opscode-sign.el")
+;; Set your username and location of the request signing key
+(setq opscode-userid "skeptomaijun21")
+(setq opscode-userpem "/Users/cb/.chef/cb-user.pem")
+;; Tell the edited version of http-twiddle to use Opscode's signing functions
+(setq http-twiddle-signing-method 'opscode-sign-buffer-request)
+;; Make sure http-twiddle-mode gets loaded
+(autoload 'http-twiddle-mode "http-twiddle" "HTTP Twiddle Mode" t nil)
+(add-to-list 'auto-mode-alist '("\\.http-twiddle$" . http-twiddle-mode))
